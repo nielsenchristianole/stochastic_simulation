c76797dcf211b5cb3ba350f7f294dcbc4a9104ce
Christian <nielsenchristianole@gmail.com>
2024-06-21 13:06:56 +0200

    added additional models to init

--- project2/src/models/__init__.py
+++ project2/src/models/__init__.py
@@ -1,6 +1,6 @@
 from .SIR import SIR
 from .CTMC import CTMC
-from .ctmc_model_factories import Q_SIR
+from .ctmc_model_factories import Q_SIR, Q_SIS, Q_SIRVD, Q_custom
 
 
-__all__ = ['SIR', 'CTMC', 'Q_SIR']
+__all__ = ['SIR', 'CTMC', 'Q_SIR', 'Q_SIS', 'Q_SIRVD', 'Q_custom']
